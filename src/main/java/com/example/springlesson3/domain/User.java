f6b7eaea97acbbf9268835a4ee7c2b0b261a1a03
kruto <krutoi.chuvak1918@gmail.com>
2024-10-31 21:06:03 +0300

    SpringLesson4 + Fix

--- src/main/java/com/example/springlesson3/domain/User.java
+++ src/main/java/com/example/springlesson3/domain/User.java
@@ -1,47 +1,15 @@
 package com.example.springlesson3.domain;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "users")
+@Data
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
-
-    public User() {
-    }
-
-    public User(Long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
