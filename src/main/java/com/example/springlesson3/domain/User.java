7f91383866f475954027198feb1265d62c465f00
kruto <krutoi.chuvak1918@gmail.com>
2024-10-31 21:07:26 +0300

    SpringLesson4 + Fix

--- src/main/java/com/example/springlesson3/domain/User.java
+++ src/main/java/com/example/springlesson3/domain/User.java
@@ -1,15 +1,21 @@
 package com.example.springlesson3.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Table(name = "users")
 @Data
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
+
+    public User() {
+
+    }
 }
